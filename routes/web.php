e6ac1d306941446fa5ccf8d3f4a4d1a179e8e5a7
Your Name <you@example.com>
2025-09-29 17:15:52 +0330

    update routes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
